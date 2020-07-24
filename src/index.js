e31e761958a74b2cffe78401996f8f28bbde74b8
Eric Peterson <ericjamesp.2009@gmail.com>
2020-07-24 13:44:44 -0500

    actions...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import store from "./store";
 
+const unsubscribe = store.subscribe(() => {
+  console.log(
+    "Store changed! (This is where react re-renders UI)",
+    store.getState()
+  );
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "Bug1",
   },
 });
+
+unsubscribe();
 
 store.dispatch({
   type: "bugRemoved",
